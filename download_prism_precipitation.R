b2950cdd4386e7751cf489acb058053ef445a245
Sara <sara_varela@yahoo.com>
2014-08-05 16:49:20 -0700

    added trend analysis

--- download_prism_precipitation.R
+++ download_prism_precipitation.R
@@ -16,7 +16,7 @@ library (maptools)
 # get a list of the files we should download from prism ftp
 
 setwd("C:/Users/visitor/Sara/Drought/ppt")
-for (year in 2000:2013){
+for (year in 1981:2013){
 url<- paste ("ftp://prism.nacse.org/monthly/ppt/", year, "/", sep="")
 items <- strsplit(getURL(url, .opts=curlOptions(ftplistonly=TRUE)), "\r\n")[[1]] 
 filename<- items [grep(paste (year, "_bil", sep=""), items)]
@@ -27,11 +27,7 @@ unzip (filename)
 
 # mask the usa maps with california to obtain a map of california
 cali<- readShapeSpatial ("C:/Users/visitor/Sara/Drought/USA_adm1.shp")
-class (cali)
-names (cali)
-head (cali)
 california<- cali [cali$NAME_1 == "California", ]
-plot (california)
 
 #list all the bil files to open, open, crop and make a stack
 name<- list.files ("C:/Users/visitor/Sara/Drought/ppt", pattern="bil.bil")
@@ -45,4 +41,74 @@ mapas<- stack (mapas, mapa)
 }
 
 plot (mapas [[2]], xlim =c(-125, -112), ylim =c(30, 45))
+mean_precip<- mean (mapas)
+plot (mean_precip, xlim =c(-125, -112), ylim =c(30, 45))
+media<- mean (mean_precip@data@values, na.rm=TRUE)
+dim (mapas)
 
+diff<- NULL
+for (i in 1:dim (mapas)[3]){
+  mapa2<- mapas [[i]]
+  media1<- mean (getValues (mapa2), na.rm=TRUE)
+  dif <- c(media1 - media )
+  diff<- c(diff, dif)
+}
+
+plot (diff, type="l", axes= F, xlim= c(0, 33), 
+      ylab= "Anomalies in precipitation (mm)", xlab="Years")
+abline (h=0, col="grey")
+axis(1, labels=c(1981:2013), at=c(1:33), las=2)
+axis (2)
+
+library (ggplot2)
+data<- data.frame (Years= c(1981:2013), Diff_precip=diff)
+names (data)
+qplot(Years, Diff_precip, data=data, 
+      geom=c("point", "smooth"),
+      method="lm", formula=y~x,
+      main="PRISM precipitation",
+      xlab="Years", ylab="Anomalies in precipitation (mm)")
+
+
+plot (mapas, xlim =c(-125, -112), 
+      ylim =c(30, 45), zlim= c(0, 5000), 
+      axes=F, box=F, main="")
+
+
+##### MannKendall test
+install.packages ("Kendall")
+library(ggplot2)
+library(Kendall)
+library(plyr)
+library(mgcv)
+
+
+coord<- xyFromCell (mapas [[1]], 1:ncell(mapas [[1]]))
+res<- coord
+for (i in 1:dim (mapas)[3]){
+  mapa2<- mapas [[i]]
+  datos<- getValues (mapa2)
+  res<- cbind (res, datos)
+}
+
+res2<- res [-c(which (is.na (res[,3]))),]
+
+
+names (res2)<-c(1981:2013) 
+dim (res2)
+
+trends<- NULL
+for (i in 1:nrow (res2)){
+  mk <- MannKendall(res2 [i,-c(1:2)])
+  resul<- c(tau= round (mk$tau [1], 3), p= round (mk$sl [1], 4))
+  trends<- rbind(trends, resul)
+}
+
+colnames (trends)<- c("tau", "p")
+trends_maps<- cbind (res2[,c(1:2)], trends)
+head (trends_maps)
+tau<- rasterFromXYZ (trends_maps[,c(1:3)])
+plot (tau)
+
+p<- rasterFromXYZ (trends_maps[,c(-3)])
+plot (p)
